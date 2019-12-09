92af0d497e2cd4516ec8b1dffb8a566d25335673
Anurag <anurag@Tamannas-MacBook-Pro.local>
2019-12-08 20:56:37 -0800

    Updates

---
+++ precache-manifest.0cc6f0418b2c3626a64fa16a2efc774b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8150e9be00dfb30ccf0b2876eb6fe14",
+    "url": "/CustomerTrans/index.html"
+  },
+  {
+    "revision": "d7fc3ade2c16ced100da",
+    "url": "/CustomerTrans/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f708a924706e4c29b4f6",
+    "url": "/CustomerTrans/static/css/main.ba7d41f2.chunk.css"
+  },
+  {
+    "revision": "d7fc3ade2c16ced100da",
+    "url": "/CustomerTrans/static/js/2.36874bfb.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/CustomerTrans/static/js/2.36874bfb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f708a924706e4c29b4f6",
+    "url": "/CustomerTrans/static/js/main.3ef9ebfe.chunk.js"
+  },
+  {
+    "revision": "666501adcc73fc164777",
+    "url": "/CustomerTrans/static/js/runtime-main.36773ba7.js"
+  }
+]);
